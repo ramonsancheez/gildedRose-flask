af36260c8e17a6e93afc6144911ef30d6ce5784c
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-03-09 20:56:59 +0100

    fix: :bug: import certifi

--- repository/db_atlas.py
+++ repository/db_atlas.py
@@ -1,10 +1,11 @@
 from repository.db_uri import db_uri
 from mongoengine import connect
+import certifi
 
 class db_atlas:
 
     @staticmethod
     def connectDB():
-        # ca = certifi.where(), tlsCAFile=ca
-        conexion = connect(host=db_uri)
+        ca = certifi.where() 
+        conexion = connect(host=db_uri, tlsCAFile=ca)
         return conexion["ollivanders"]["ollivanders"]
