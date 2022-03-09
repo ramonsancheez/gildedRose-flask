724fb13e52a13ffa73cb7dffd907a802d9716522
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-03-09 11:59:14 +0100

    feat: :sparkles: simulate bd

---
+++ repository/bd.py
@@ -0,0 +1,6 @@
+from stock import stock
+
+class bd:
+    @staticmethod
+    def get_stock():
+        return stock
---
+++ repository/stock.py
@@ -0,0 +1,22 @@
+stock = [
+    {
+        "name": "Aged_brie",
+        "quality": 3,
+        "sell_in": 4,
+    },
+    {
+        "name": "Sulfuras",
+        "quality": 80,
+        "sell_in": 0,
+    },
+    {
+        "name": "Aged_brie",
+        "quality": 12,
+        "sell_in": 0,
+    },
+    {
+        "name": "Conjured",
+        "quality": 6,
+        "sell_in": 10,
+    },
+]
