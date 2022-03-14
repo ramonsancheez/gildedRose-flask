6a13694205b192f9bea6b8b4d9a4ef22ecf5aef6
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-03-14 11:41:05 +0100

    fix: :bug: fix deletedItem uri

--- api.py
+++ api.py
@@ -5,20 +5,19 @@ from resources.inventario import inventario
 from resources.nombre import nombre
 from resources.quality import quality
 from resources.sell_in import sell_in
-from resources.delete import delete
+from resources.deletedItem import deletedItem
+from resources.newItem import newItem
 
 app = Flask(__name__)
 api = Api(app, catch_all_404s=True)
-
 
 api.add_resource(index, "/")
 api.add_resource(inventario, "/inventario")
 api.add_resource(nombre, "/nombre/<itemNombre>")
 api.add_resource(quality, "/quality/<numQuality>")
 api.add_resource(sell_in, "/sell_in/<numSellIn>")
-api.add_resource(delete, "/delete/<deletedItemName>")
-
-
+api.add_resource(deletedItem, "/deletedItem/<deletedItemName>")
+api.add_resource(newItem, "/newItem/<name>/<quality>/<sell_in>")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
