240fce06b6b44abd0f0a23880bf9af2664397f4f
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-03-17 15:58:12 +0100

    refactor: :fire: unnecessary file

--- resources/delete.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Resource
-from services.service  import service
-
-class delete(Resource):
-    def get(self, deletedItemName):
-        return service.delete_item(deletedItemName)
